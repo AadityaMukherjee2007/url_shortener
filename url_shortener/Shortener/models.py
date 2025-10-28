d7db000bda63d0730ee2ff3b58a37be774bd8ebd
Arman <halimarman2007@gmail.com>
2025-10-28 16:45:25 +0530

    retry

--- url_shortener/Shortener/models.py
+++ url_shortener/Shortener/models.py
@@ -1,29 +1,28 @@
 from django.db import models
-
-# Create your models here.
-import string , random
-
+from django.utils import timezone
+import string, random
 
 def generate_slug(length=6):
     characters = string.ascii_letters + string.digits
-    slug = ''.join(random.choice(characters) for _ in range(length))
-    return slug
-
+    return ''.join(random.choice(characters) for _ in range(length))
 
 class Link(models.Model):
     original_url = models.URLField()
-    slug = models.SlugField(max_length=15, unique=True, default=generate_slug)
+    slug = models.SlugField(unique=True, max_length=15, blank=True)
+    clicks = models.PositiveIntegerField(default=0)
+    last_accessed = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = generate_slug()
-            while Link.objects.filter(slug=self.slug).exists():
+            max_attempts = 10
+            for attempt in range(max_attempts):
                 self.slug = generate_slug()
-
+                if not Link.objects.filter(slug=self.slug).exists():
+                    break
+            else:
+                raise ValueError("Could not generate a unique slug after multiple attempts.")
         super().save(*args, **kwargs)
 
-        
     def __str__(self):
-        return f"{self.original_url} -> {self.slug}"
+        return f"{self.slug} -> {self.original_url}"
